e7b48da1bd0caa9e7da97c681b392bcabb3779a0
vio1990 <vio1990@ukr.net>
2016-10-13 15:17:03 +0300

    ProfileService implementation class has been done.

--- src/main/java/com/ozerian/app/service/imp/ProfileServiceImpl.java
+++ src/main/java/com/ozerian/app/service/imp/ProfileServiceImpl.java
@@ -3,25 +3,39 @@ package com.ozerian.app.service.imp;
 import com.ozerian.app.entity.Profile;
 import com.ozerian.app.repository.ProfileRepository;
 import com.ozerian.app.service.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+/**
+ * Implemented class of appropriate Service class interface
+ * for possibility to add, delete, findAll Profiles.
+ */
+@Service
 public class ProfileServiceImpl implements ProfileService {
 
     private ProfileRepository profileRepository;
 
     @Override
     public Profile addProfile(Profile profile) {
-        return null;
+        profileRepository.save(profile);
+        return profile;
     }
 
     @Override
     public void deleteProfile(Long id) {
-
+        profileRepository.delete(id);
     }
 
     @Override
     public List<Profile> getAllProfiles() {
-        return null;
+        return (List<Profile>) profileRepository.findAll();
+    }
+
+    @Autowired
+    public void setProfileRepository(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
     }
 }
