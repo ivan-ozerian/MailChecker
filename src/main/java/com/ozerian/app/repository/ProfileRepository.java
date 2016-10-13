239ffed92d173437c8019e7c8204da9d9cdeb053
vio1990 <vio1990@ukr.net>
2016-10-13 15:03:51 +0300

    ProfileRepository has been done.

--- src/main/java/com/ozerian/app/repository/ProfileRepository.java
+++ src/main/java/com/ozerian/app/repository/ProfileRepository.java
@@ -3,6 +3,9 @@ package com.ozerian.app.repository;
 import com.ozerian.app.entity.Profile;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Spring Data JPA Repository for manipulation with Letter's entity.
+ */
 public interface ProfileRepository extends CrudRepository<Profile, Long> {
 
 }
