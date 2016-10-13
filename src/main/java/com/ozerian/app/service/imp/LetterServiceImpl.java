e076773f0d9eac792505d9d925108185486f6512
vio1990 <vio1990@ukr.net>
2016-10-13 15:16:17 +0300

    Letter Service implementation has been done.

--- src/main/java/com/ozerian/app/service/imp/LetterServiceImpl.java
+++ src/main/java/com/ozerian/app/service/imp/LetterServiceImpl.java
@@ -3,25 +3,38 @@ package com.ozerian.app.service.imp;
 import com.ozerian.app.entity.Letter;
 import com.ozerian.app.repository.LetterRepository;
 import com.ozerian.app.service.LetterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Implemented class of appropriate Service class interface
+ * for possibility to add, delete, findAll Letters.
+ */
+@Service
 public class LetterServiceImpl implements LetterService {
 
     private LetterRepository letterRepository;
 
     @Override
     public Letter addLetter(Letter letter) {
-        return null;
+        letterRepository.save(letter);
+        return letter;
     }
 
     @Override
     public void deleteLetter(Long id) {
-
+        letterRepository.delete(id);
     }
 
     @Override
     public List<Letter> getAllLetters() {
-        return null;
+        return (List<Letter>) letterRepository.findAll();
+    }
+
+    @Autowired
+    public void setLetterRepository(LetterRepository letterRepository) {
+        this.letterRepository = letterRepository;
     }
 }
