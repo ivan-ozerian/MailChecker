5d901642aa22a9dd66d1f604a0e8304b93cdadba
vio1990 <vio1990@ukr.net>
2016-10-13 19:34:15 +0300

    Addition of logging to the service.

--- src/main/java/com/ozerian/app/service/imp/LetterServiceImpl.java
+++ src/main/java/com/ozerian/app/service/imp/LetterServiceImpl.java
@@ -3,6 +3,7 @@ package com.ozerian.app.service.imp;
 import com.ozerian.app.model.entity.Letter;
 import com.ozerian.app.repository.LetterRepository;
 import com.ozerian.app.service.LetterService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,20 +18,26 @@ public class LetterServiceImpl implements LetterService {
 
     private LetterRepository letterRepository;
 
+    private final static Logger LOGGER = Logger.getLogger(LetterServiceImpl.class);
+
     @Override
     public Letter saveLetter(Letter letter) {
         letterRepository.save(letter);
+        LOGGER.debug("saveLetter() service method was called and execute success");
         return letter;
     }
 
     @Override
     public void deleteLetter(Long id) {
         letterRepository.delete(id);
+        LOGGER.debug("deleteLetter() service method was called and execute success");
     }
 
     @Override
     public List<Letter> getAllLetters() {
-        return (List<Letter>) letterRepository.findAll();
+        List<Letter> letters = (List<Letter>) letterRepository.findAll();
+        LOGGER.debug("getAllLetter() service method was called and execute success");
+        return letters;
     }
 
     @Autowired
--- src/main/java/com/ozerian/app/service/imp/ProfileServiceImpl.java
+++ src/main/java/com/ozerian/app/service/imp/ProfileServiceImpl.java
@@ -3,6 +3,7 @@ package com.ozerian.app.service.imp;
 import com.ozerian.app.model.entity.Profile;
 import com.ozerian.app.repository.ProfileRepository;
 import com.ozerian.app.service.ProfileService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,20 +19,26 @@ public class ProfileServiceImpl implements ProfileService {
 
     private ProfileRepository profileRepository;
 
+    private final static Logger LOGGER = Logger.getLogger(ProfileServiceImpl.class);
+
     @Override
     public Profile saveProfile(Profile profile) {
         profileRepository.save(profile);
+        LOGGER.debug("saveProfile() service method was called and execute success");
         return profile;
     }
 
     @Override
     public void deleteProfile(Long id) {
         profileRepository.delete(id);
+        LOGGER.debug("deleteProfile() service method was called and execute success");
     }
 
     @Override
     public List<Profile> getAllProfiles() {
-        return (List<Profile>) profileRepository.findAll();
+        List<Profile> profiles = (List<Profile>) profileRepository.findAll();
+        LOGGER.debug("getAllProfiles() service method was called and execute success");
+        return profiles;
     }
 
     @Autowired
