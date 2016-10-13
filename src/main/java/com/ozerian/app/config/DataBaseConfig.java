31b303b56b947a3e2ea771dd78f2edf2b2b321f5
vio1990 <vio1990@ukr.net>
2016-10-13 14:45:50 +0300

    DataSource, BeanManager, TransactionalManager has been configured.

---
+++ src/main/java/com/ozerian/app/config/DataBaseConfig.java
@@ -0,0 +1,82 @@
+package com.ozerian.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * Class is for configuration of connection to H2 Data base and configuration
+ * set properties for EntityManager, Data Source and transactional support.
+ */
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories("com.ozerian.app.repository")
+public class DataBaseConfig {
+
+    /**
+     * Data source configuration.
+     *
+     * @param env Environment environment in woch application is running.
+     * @return DataSource configured DataSource object.
+     */
+    @Bean
+    DataSource dataSource(Environment env) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getRequiredProperty("db.driver"));
+        dataSource.setUrl(env.getRequiredProperty("db.url"));
+        dataSource.setUsername(env.getRequiredProperty("db.username"));
+        dataSource.setPassword(env.getRequiredProperty("db.password"));
+
+        return dataSource;
+    }
+
+    /**
+     * Configuration of FactoryBeanManager.
+     *
+     * @param dataSource DataSource configured object.
+     * @param env Environment environment in wich application is running.
+     * @return LocalContainerEntityManagerFactoryBean bean manager.
+     */
+    @Bean
+    LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource,
+                                                                Environment env) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactoryBean.setPackagesToScan("com.ozerian.app.entity");
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
+        jpaProperties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        jpaProperties.put("hibernate.ejb.naming_strategy", env.getRequiredProperty("hibernate.ejb.naming_strategy"));
+        jpaProperties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+        jpaProperties.put("hibernate.format_sql", env.getRequiredProperty("hibernate.format_sql"));
+        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+
+        return entityManagerFactoryBean;
+    }
+
+    /**
+     * Configuration of transactional support.
+     *
+     * @param entityManagerFactory EntityManagerFactory for configuration.
+     * @return JpaTransactionManager configured object.
+     */
+    @Bean
+    JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
+}
